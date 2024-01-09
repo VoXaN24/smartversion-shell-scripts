56b0d4286b92973a1a2e92928996be0939b237f8
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-01-09 23:06:23 +0100

    Update patch-all.bat

--- patch-all.bat
+++ patch-all.bat
@@ -1,7 +1,8 @@
 @echo off
+setlocal enabledelayedexpansion
 
 if not exist "smv.exe" (
-    echo error: Could not find SmartVersion executable (smv.exe) in the current directory.
+    echo "error: Could not find SmartVersion executable (smv.exe) in the current directory."
     exit /b 1
 )
 
@@ -18,22 +19,25 @@ if not exist "%1" (
     exit /b 1
 )
 
-for %%f in (*.%~x1) do (
-    if /i "%%f" neq "%1" (
-        .\smv.exe BuildPatch "%%~nf.svf" %1 "%%f" -nbhashbits 24 -compressratio 192 -sha1 -sha256
-        if %errorlevel% equ 0 (
-            set /a success+=1
-        ) else (
-            set /a error+=1
+for %%f in (*.*) do (
+    if /i "%%f" neq "smv.exe" (
+        if /i "%%f" neq "%~nx1" (
+            if not exist "%%~nf_to_%~n1.svf" (
+                .\smv.exe BuildPatch "%%~nf_to_%~n1.svf" "%~nx1" "%%f" -nbhashbits 24 -compressratio 192 -sha1 -sha256
+                if !errorlevel! equ 0 (
+                    set /a success+=1
+                ) else (
+                    set /a error+=1
+                )
+            )
         )
     )
 )
 
-if %error% equ 0 (
-    echo success: Successfully patched %success% files.
+if !error! equ 0 (
+    echo success: Successfully patched !success! files.
     exit /b 0
 ) else (
-    echo error: Successfully patched %success% files and encountered %error% errors.
+    echo error: Successfully patched !success! files and encountered !error! errors.
     exit /b 1
 )
-
